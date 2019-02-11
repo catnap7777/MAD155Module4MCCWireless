7988acdd1c8fc034ac80a740c6792bdb0c90fce0
kam <kmathes348@students.mchenry.edu>
2019-02-10 19:45:46 -0600

    more code additions

---
+++ app/src/main/java/mad155/kmathes/mad155module4mccwireless/ChooseDeal.java
@@ -0,0 +1,13 @@
+package mad155.kmathes.mad155module4mccwireless;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseDeal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_deal);
+    }
+}
