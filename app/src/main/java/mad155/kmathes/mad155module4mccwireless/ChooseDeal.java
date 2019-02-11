bf3c27749ccef5f0e0726d55072eeaf57d8d311f
kam <kmathes348@students.mchenry.edu>
2019-02-10 23:02:14 -0600

    more code additions and changes

--- app/src/main/java/mad155/kmathes/mad155module4mccwireless/ChooseDeal.java
+++ app/src/main/java/mad155/kmathes/mad155module4mccwireless/ChooseDeal.java
@@ -9,5 +9,20 @@ public class ChooseDeal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_deal);
+
+        String plan = "";
+        Bundle extras = getIntent().getExtras();
+        if(extras != null){
+            plan = extras.getString("planSelected");
+        }
+
+        System.out.println("plan that was selected: " + plan);
+
+        // Check to make sure project isn't null for some reason
+        if(plan != null && !plan.isEmpty()) {
+
+            System.out.println("Project is not empty or null : " + plan);
+            // For Bicycle...
+        }
     }
 }
--- app/src/main/java/mad155/kmathes/mad155module4mccwireless/MainActivity.java
+++ app/src/main/java/mad155/kmathes/mad155module4mccwireless/MainActivity.java
@@ -1,7 +1,11 @@
 package mad155.kmathes.mad155module4mccwireless;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        Button btnPlan = (Button) findViewById(R.id.btnChoosePlan);
+
+        btnPlan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String selectedPlan;
+
+                final RadioButton rbBasic = (RadioButton) findViewById(R.id.rbBasicPlan);
+                final RadioButton rbMid = (RadioButton) findViewById(R.id.rbMidPlan);
+                final RadioButton rbPremium = (RadioButton) findViewById(R.id.rbPremiumPlan);
+
+                if (rbBasic.isChecked()) {
+                    selectedPlan = "Basic";
+                } else if (rbMid.isChecked()) {
+                    selectedPlan = "Middle";
+                } else if (rbPremium.isChecked()) {
+                    selectedPlan = "Premium";
+                } else {
+                    selectedPlan = "Wrong";
+                }
+
+                Intent intent1 = new Intent(MainActivity.this, ChooseDeal.class);
+                intent1.putExtra("planSelected", selectedPlan);
+                startActivity(intent1);
+            }
+        });
+
     }
 }
