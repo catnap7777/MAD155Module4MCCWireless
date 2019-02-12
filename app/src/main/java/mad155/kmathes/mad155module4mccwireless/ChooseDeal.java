c021c848460907d618123a87f0e96e54911eaa14
kam <kmathes348@students.mchenry.edu>
2019-02-12 13:16:50 -0600

    more code4

--- app/src/main/java/mad155/kmathes/mad155module4mccwireless/ChooseDeal.java
+++ app/src/main/java/mad155/kmathes/mad155module4mccwireless/ChooseDeal.java
@@ -1,5 +1,7 @@
 package mad155.kmathes.mad155module4mccwireless;
 
+import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,13 +20,16 @@ public class ChooseDeal extends AppCompatActivity {
         setContentView(R.layout.activity_choose_deal);
 
 
-        String plan = "";
-        String planDesc = "";
+        final String plan;
+        final String planDesc;
 
         Bundle extras = getIntent().getExtras();
         if(extras != null){
             plan = extras.getString("planSelected");
             planDesc = extras.getString("planSelectedDesc");
+        } else {
+            plan = "";
+            planDesc = "";
         }
 
         System.out.println("plan that was selected: " + plan);
@@ -51,7 +56,9 @@ public class ChooseDeal extends AppCompatActivity {
                 public void onClick(View v) {
 
                     String qtyIphone = "";
+                    int qtyIphone2 = 0;
                     String qtyGalaxy = "";
+                    int qtyGalaxy2 = 0;
                     String descIphone = "";
                     String descGalaxy = "";
                     boolean passIphone = false;
@@ -59,12 +66,14 @@ public class ChooseDeal extends AppCompatActivity {
 
                     if (cBoxIphone.isChecked() && spnrIphone.getSelectedItemPosition() > 0) {
                         qtyIphone = spnrIphone.getSelectedItem().toString();
+                        qtyIphone2 = Integer.parseInt(spnrIphone.getSelectedItem().toString());
                         descIphone = getString(R.string.cbIPhoneXR);
                         passIphone = true;
                     }
 
                     if (cBoxGalaxy.isChecked() && spnrGalaxy.getSelectedItemPosition() > 0) {
                         qtyGalaxy = spnrGalaxy.getSelectedItem().toString();
+                        qtyGalaxy2 = Integer.parseInt(spnrGalaxy.getSelectedItem().toString());
                         descGalaxy = getString(R.string.cbGalaxy9S);
                         passGalaxy = true;
 
@@ -107,12 +116,25 @@ public class ChooseDeal extends AppCompatActivity {
                     });
 
 
-                    System.out.println("passIphone: " + passIphone + " qtyIphone: " + qtyIphone);
+                    System.out.println("passIphone: " + passIphone + " qtyIphone: " + qtyIphone2);
                     System.out.println("descIphone: " + descIphone);
-                    System.out.println("passGalaxy: " + passGalaxy + " qtyGalaxy: " + qtyGalaxy);
+                    System.out.println("passGalaxy: " + passGalaxy + " qtyGalaxy: " + qtyGalaxy2);
                     System.out.println("descGalaxy: " + descGalaxy);
 
-
+                    // if passIphone true - build for intent
+                    // if passGalaxy true - build for intent
+                    // don't forget to pass plan
+                    ///... or maybe i just sent everything and check on next screen?
+                    Intent intent2 = new Intent(ChooseDeal.this, DisplaySummary.class);
+                    intent2.putExtra("planSelected", plan);
+                    intent2.putExtra("planSelectedDesc", planDesc);
+                    intent2.putExtra("passIphone", passIphone);
+                    intent2.putExtra("descIphone", descIphone);
+                    intent2.putExtra("qtyIphone", qtyIphone2);
+                    intent2.putExtra("passGalaxy", passGalaxy);
+                    intent2.putExtra("descGalaxy", descGalaxy);
+                    intent2.putExtra("qtyGalaxy", qtyGalaxy2);
+                    startActivity(intent2);
 
                 }
             });
--- app/src/main/java/mad155/kmathes/mad155module4mccwireless/DisplaySummary.java
+++ app/src/main/java/mad155/kmathes/mad155module4mccwireless/DisplaySummary.java
@@ -2,6 +2,9 @@ package mad155.kmathes.mad155module4mccwireless;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class DisplaySummary extends AppCompatActivity {
 
@@ -9,5 +12,64 @@ public class DisplaySummary extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_summary);
+
+        final String SummaryPlan;
+        final String SummaryPlanDesc;
+        final boolean SummaryPassIphone;
+        final String SummaryDescIphone;
+        //final String SummaryQtyIphone;
+        final int SummaryQtyIphone;
+        final boolean SummaryPassGalaxy;
+        final String SummaryDescGalaxy;
+        //final String SummaryQtyGalaxy;
+        final int SummaryQtyGalaxy;
+
+        TextView phonesSelected = (TextView) findViewById(R.id.txtPhoneAndQtySelected);
+        TextView planSelected = (TextView) findViewById(R.id.txtPlanSelected);
+        RadioButton radioPayFull = (RadioButton) findViewById(R.id.rbPayFull);
+        RadioButton radioPayMonthly = (RadioButton) findViewById(R.id.rbPayMonthly);
+        Button btnGetTotals = (Button) findViewById(R.id.btnTotals);
+        TextView txtOrderTotals = (TextView) findViewById(R.id.txtOrderTotals);
+
+        Bundle extras = getIntent().getExtras();
+        if(extras != null) {
+            SummaryPlan = extras.getString("planSelected");
+            SummaryPlanDesc = extras.getString("planSelectedDesc");
+            SummaryPassIphone = extras.getBoolean("passIphone");
+            SummaryDescIphone = extras.getString("descIphone");
+            //SummaryQtyIphone = extras.getString("qtyIphone");
+            SummaryQtyIphone = extras.getInt("qtyIphone");
+            SummaryPassGalaxy = extras.getBoolean("passGalaxy");
+            SummaryDescGalaxy = extras.getString("descGalaxy");
+            //SummaryQtyGalaxy = extras.getString("qtyGalaxy");
+            SummaryQtyGalaxy = extras.getInt("qtyGalaxy");
+        } else {
+            SummaryPlan = "";
+            SummaryPlanDesc = "";
+            SummaryPassIphone = false;
+            SummaryDescIphone = "";
+            SummaryQtyIphone = 0;
+            SummaryPassGalaxy = false;
+            SummaryDescGalaxy = "";
+            SummaryQtyGalaxy = 0;
+        }
+
+        System.out.println("SummaryPassIphone: " + SummaryPassIphone + " SummaryQtyIphone: " + SummaryQtyIphone);
+        System.out.println("SummaryDescIphone: " + SummaryDescIphone);
+        System.out.println("SummaryPassGalaxy: " + SummaryPassGalaxy + " SummaryQtyGalaxy: " + SummaryQtyGalaxy);
+        System.out.println("SummaryDescGalaxy: " + SummaryDescGalaxy);
+
+        planSelected.setText("Plan Selected: " + SummaryPlan + "\n\t" + SummaryPlanDesc);
+
+        if ((SummaryPassIphone) && (SummaryPassGalaxy)) {
+            phonesSelected.setText("Quantity: " + SummaryQtyIphone + " - \n\t\t" + SummaryDescIphone + "\n" +
+                    "Quantity: " + SummaryQtyGalaxy + " - \n\t\t" + SummaryDescGalaxy);
+        } else if ((SummaryPassIphone) && (!SummaryPassGalaxy)) {
+            phonesSelected.setText("Quantity: " + SummaryQtyIphone + " - \n\t\t" + SummaryDescIphone);
+        } else if ((!SummaryPassIphone) && (SummaryPassGalaxy)) {
+            phonesSelected.setText("Quantity: " + SummaryQtyGalaxy + " - \n\t\t" + SummaryDescGalaxy);
+        } else {
+            phonesSelected.setText("No phones were selected/ordered");
+        }
     }
 }
